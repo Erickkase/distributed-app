9efba80ef3ffd61ac4ea42e267b413cdad641b06
Erickkase <118641197+Erickkase@users.noreply.github.com>
2025-05-26 01:55:00 -0500

    back

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -5,12 +5,13 @@ const App = () => {
   const [message, setMessage] = React.useState('');
 
   React.useEffect(() => {
-    fetch('/api')
+    fetch('http://44.202.23.199:3001/api') // usa tu IP real aquí
       .then(res => res.json())
       .then(data => setMessage(data.message));
   }, []);
 
-  return <h1>{message || "Desplegando con GitHub Actions..."}</h1>;
+  return <h1>{message || "Cargando desde el backend..."}</h1>;
+}
 
 }
 
